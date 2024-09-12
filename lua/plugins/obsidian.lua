db5cbf9e8b84c21384226fdc9e65e87bce1c8804
WMinerva <nicolas.ochoa1697@gmail.com>
2024-09-12 15:41:53 -0400

    manj ac

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -10,25 +10,25 @@ return {
     "hrsh7th/nvim-cmp",
   },
 
-  opts = {
-    workspaces = {
-      {
-        name = "universidad",
-        path = "~/Escritorio/zettelkasten/",
-      },
-      -- {
-      --   name = "133",
-      --   path = "~/Escritorio/zettelkasten/133/",
-      -- },
-      -- {
-      --   name = "121",
-      --   path = "~/Escritorio/zettelkasten/121/",
-      -- },
-    },
-    -- templates = {
-    --   subdir = "templates",
-    --   date_format = "%Y-%m-%d-%a",
-    --   time_format = "%H:%M",
-    -- },
-  },
+  -- opts = {
+  --   workspaces = {
+  --     {
+  --       name = "universidad",
+  --       path = "~/Escritorio/zettelkasten/",
+  --     },
+  --     -- {
+  --     --   name = "133",
+  --     --   path = "~/Escritorio/zettelkasten/133/",
+  --     -- },
+  --     -- {
+  --     --   name = "121",
+  --     --   path = "~/Escritorio/zettelkasten/121/",
+  --     -- },
+  --   },
+  --   -- templates = {
+  --   --   subdir = "templates",
+  --   --   date_format = "%Y-%m-%d-%a",
+  --   --   time_format = "%H:%M",
+  --   -- },
+  -- },
 }
